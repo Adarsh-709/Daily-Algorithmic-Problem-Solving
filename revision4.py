a415fe7af276f870466137e78cd77f19b0b3c860
Adarsh-709 <sharmaadus098@gmail.com>
2026-05-06 23:49:23 +0530

    revision 4

--- revision4.py
+++ revision4.py
@@ -7,7 +7,37 @@
 
 class Solution:
     def pattern(self, n: int):
-        pass
+        mat = [[0]*n for _ in range(n)]
+        top = 0
+        bottom = n-1
+        left = 0
+        right = n-1
+        num = 1
+        while top <= bottom and left <= right:
+            for i in range(top,right+1):
+                mat[top][i] = num
+                num += 1
+            top+=1
+            for j in range(top, bottom+1):
+                mat[j][right] = num
+                num+=1 
+            right -= 1
+
+            if top<=bottom:
+                for i in range(right,left-1,-1):
+                    mat[bottom][i] = num
+                    num+=1
+                bottom-=1
+            if left <= right:
+                for i in range(bottom,top-1,-1):
+                    mat[i][left]=num
+                    num+=1
+                left += 1
+
+        for i in range(n):
+            for j in range(n):
+                print(mat[i][j],end = " ")
+            print()
 
 if __name__=="__main__":
     sol = Solution()

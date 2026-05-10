804bae2f068d8a53a000f3af9fe846a5db468765
Adarsh-709 <sharmaadus098@gmail.com>
2026-05-10 22:41:22 +0530

    revison 5

--- revision5.py
+++ revision5.py
@@ -10,7 +10,15 @@
 
 class Solution:
     def pattern(self, n: int):
-        pass
+        for i in range(2*n-1):
+            for j in range(2*n-1):
+                top = i
+                bottom = 2*n-i-2
+                left = j
+                right = 2*n-j-2
+                d = min(top,bottom,left,right)
+                print(n-d, end=" ")
+            print()
 
 if __name__=="__main__":
     sol = Solution()

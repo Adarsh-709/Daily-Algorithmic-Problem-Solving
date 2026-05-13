c4acc251245cf7a3e9ac5904e018094fbbebd182
Adarsh-709 <sharmaadus098@gmail.com>
2026-05-13 23:34:49 +0530

    count prime fixed

--- BasicMathProgramming/count_prime.py
+++ BasicMathProgramming/count_prime.py
@@ -1,17 +1,15 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        ictn = 0
-        for i in range(2,n):
-            if i==2 or i == 3:
-                ictn+=1
-                continue
-            for j in range(2,int(i**0.5)+1):
-                if i%j==0:
-                    break
-                elif j==int(i**0.5):
-                    print(i)
-                    ictn+=1
-        return ictn
+        if n<2:
+            return 0
+        is_prime = [True]*n
+        is_prime[0] = is_prime[1] = False
+        for i in range(2,int(n**0.5)+1):
+            if is_prime[i]:
+                for j in range(i*i,n,i):
+                    is_prime[j]=False
+        return sum(is_prime)
+
 
 
 if __name__ == '__main__':
